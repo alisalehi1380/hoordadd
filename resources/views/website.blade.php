1e1c4634541abe7c52dfde1ddf26e17f04850fa6
Ali_Salehi <alisalehi13800930@gmail.com>
2023-07-11 17:56:53 +0330

    [Add] website front view p2

--- resources/views/website.blade.php
+++ resources/views/website.blade.php
@@ -1,38 +1,186 @@
 <!doctype html>
-<html lang="en">
+<html lang="fa" dir="rtl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>هورداد</title>
+    <title>فروشگاه اینترنتی هورداد</title>
 
-    <link rel="stylesheet" href="{{ asset('website/bootstrap/bootstrap.rtl.css') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('website/img/favicon.png') }}">
+    {{--    <meta name="description" content="فروشگاه اینترنتی هورداد | فروش آنلاین تجهیزات برق - ارسال به سراسر ایران - قیمت کنتاکتور - بانک خازنی - قیمت اینورتر">--}}
+    <meta name="description" content="هر آنچه که نیاز دارید با بهترین قیمت از فروشگاه هورداد بخرید! جدیدترین انواع کنتاکتور - بانک خازنی - اینورترر - ارسال به سراسر ایران">
+    <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap.rtl.css') }}">
     <link rel="stylesheet" href="{{ asset('website/css/styleWebsite.css') }}">
 </head>
+<style>
+    body {
+
+    }
+</style>
 <body>
-<section id="body" class="container">
-    <section>
-        <nav>
-            <div>
-                <a><img src="#"></a>
-                <input value="s">
+<section id="navbar" class="container-fluid mb-4">
+    <nav>
+        <div>
+            <a><img src="{{ asset('website/img/logo-hoordad-nav.png') }}" style="max-width: 70px; max-height: 50px"></a>
+            <input value="s">
 
+        </div>
+        <div></div>
+    </nav>
+</section>
+<section id="category-1" class="container-fluid mb-4" style="background-color: #EFEFEF">
+    <div class="container">
+        asdfadf<br/>
+        asdfadf<br/>
+        asdfadf<br/>
+        asdfadf<br/>
+        asdfadf<br/>
+        asdfadf<br/>
+        asdfadf<br/>
+    </div>
+</section>
+<section id="newest-products" class="container mb-4">
+    <div class="container">
+        <p>جدید ترین محصولات</p>
+        <hr/>
+        <div class="row justify-content-center">
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/newest-products/doshakh-400x400.jpg') }}" alt="product->title">
+                </div>
+                <p>دوشاخ نری سیار پارت الکتریک مدل 9128</p>
+                <p style="color: red">قیمت : 300,000 تومان</p>
             </div>
-            <div></div>
-        </nav>
-    </section>
-
-    <section id="content">
-        <div id="category">
-
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/newest-products/doshakh-400x400.jpg') }}" alt="product->title">
+                </div>
+                <p>دوشاخ نری سیار پارت الکتریک مدل 9128</p>
+                <p style="color: red">قیمت : 300,000 تومان</p>
+            </div>
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/newest-products/doshakh-400x400.jpg') }}" alt="product->title">
+                </div>
+                <p>دوشاخ نری سیار پارت الکتریک مدل 9128</p>
+                <p style="color: red">قیمت : 300,000 تومان</p>
+            </div>
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/newest-products/doshakh-400x400.jpg') }}" alt="product->title">
+                </div>
+                <p>دوشاخ نری سیار پارت الکتریک مدل 9128</p>
+                <p style="color: red">قیمت : 300,000 تومان</p>
+            </div>
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/newest-products/doshakh-400x400.jpg') }}" alt="product->title">
+                </div>
+                <p>دوشاخ نری سیار پارت الکتریک مدل 9128</p>
+                <p style="color: red">قیمت : 300,000 تومان</p>
+            </div>
         </div>
-
-        <div id="newest-products">
-
+    </div>
+</section>
+<section id="category-2" class="container-fluid d-flex justify-content-center mb-4" style="background-color: #EFEFEF">
+    <div class="container row text-center" style="padding: 2.5rem 0;">
+        <a class="col-md-2" href="#">
+            <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+            <p class="m-0">لامپ</p>
+        </a><a class="col-md-2" href="#">
+            <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+            <p class="m-0">لامپ</p>
+        </a><a class="col-md-2" href="#">
+            <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+            <p class="m-0">لامپ</p>
+        </a><a class="col-md-2" href="#">
+            <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+            <p class="m-0">لامپ</p>
+        </a><a class="col-md-2" href="#">
+            <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+            <p class="m-0">لامپ</p>
+        </a><a class="col-md-2" href="#">
+            <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+            <p class="m-0">لامپ</p>
+        </a>
+    </div>
+</section>
+<section id="best-selling" class="container-fluid mb-4">
+    <div class="container">
+        <p>پر فروش ترین محصولات</p>
+        <hr/>
+        <div class="row justify-content-center">
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/best-selling/lamp-400-400.jpg') }}">
+                </div>
+                <p>لامپ 200 وات</p>
+                <p style="color: red">قیمت : 230,000 تومان</p>
+            </div>
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/best-selling/lamp-400-400.jpg') }}">
+                </div>
+                <p>لامپ 200 وات</p>
+                <p style="color: red">قیمت : 230,000 تومان</p>
+            </div>
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/best-selling/lamp-400-400.jpg') }}">
+                </div>
+                <p>لامپ 200 وات</p>
+                <p style="color: red">قیمت : 230,000 تومان</p>
+            </div>
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/best-selling/lamp-400-400.jpg') }}">
+                </div>
+                <p>لامپ 200 وات</p>
+                <p style="color: red">قیمت : 230,000 تومان</p>
+            </div>
+            <div class="col-md-2 mx-1" style="border: 1px solid; border-radius: 5px">
+                <div class="row">
+                    <img style="max-height: 230px; max-width: 230px;" src="{{ asset('website/img/products/best-selling/lamp-400-400.jpg') }}">
+                </div>
+                <p>لامپ 200 وات</p>
+                <p style="color: red">قیمت : 230,000 تومان</p>
+            </div>
         </div>
-    </section>
+    </div>
+</section>
+<section id="working-brands" class="container-fluid d-flex justify-content-center mb-4">
+    <div class="container text-center">
+        <p class="mb-3">برند هایی که کار میکنیم</p>
+        <hr/>
+        <div class="row" style="padding: 2.5rem 0;">
+            <a class="col-md-2" href="#">
+                <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+                <p class="m-0">لامپ</p>
+            </a>
+            <a class="col-md-2" href="#">
+                <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+                <p class="m-0">لامپ</p>
+            </a>
+            <a class="col-md-2" href="#">
+                <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+                <p class="m-0">لامپ</p>
+            </a>
+            <a class="col-md-2" href="#">
+                <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+                <p class="m-0">لامپ</p>
+            </a>
+            <a class="col-md-2" href="#">
+                <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+                <p class="m-0">لامپ</p>
+            </a>
+            <a class="col-md-2" href="#">
+                <img src="{{ asset('website/img/category/1/light-switch.png') }}" alt="product->title">
+                <p class="m-0">لامپ</p>
+            </a>
+        </div>
+    </div>
 </section>
 
-<script src="{{ asset('website/bootstrap/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('bootstrap/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
