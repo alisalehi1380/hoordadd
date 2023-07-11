2bb17ab882122c7710fbec181c69e66d79fedfcf
Ali_Salehi <alisalehi13800930@gmail.com>
2023-07-11 23:31:00 +0330

    [Add] website front view p3

--- resources/views/website.blade.php
+++ resources/views/website.blade.php
@@ -18,6 +18,7 @@
     }
 </style>
 <body>
+
 <section id="navbar" class="container-fluid mb-4">
     <nav>
         <div>
@@ -179,6 +180,105 @@
             </a>
         </div>
     </div>
+</section>
+<section id="footer" class="container-fluid d-flex justify-content-center" style="background-color: #EFEFEF">
+    <footer class="row container" style="padding: 2.5rem 0;">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="text-center mb-3">
+                    <img src="{{ asset('website/img/logo-hoordad-company.png') }}" alt="لو گو فروشگاه اینترنتی هورداد" style="max-width: 100px; max-height: 100px;">
+                </div>
+                <div>
+                    <p class="m-0">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آیندد داشت که تمام و دشواری.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-8 row">
+                <div class="col-md-3">
+                    <a href="#" class="d-block text-center mb-4">اطلاعات بیشتر</a>
+                    <ul class="">
+                        <li><a href="#">سوالات متداول</a></li>
+                        <li><a href="#">ارتباط با ما</a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <a href="#" class="d-block text-center mb-4">دسته بندی ها</a>
+                    <ul class="">
+                        <li><a href="#">سوالات متداول</a></li>
+                        <li><a href="#">ارتباط با ما</a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3"></div>
+                <div class="social-media col-md-3 row">
+                    <div>
+                        <p>
+                            راه های ارتباطی
+                        </p>
+                        <a href="#">قزوین، چهارراه نظام وفا</a>
+                        <a href="#">hoordadco.info@gmial.com</a>
+                    </div>
+                    <div class="col-12">
+                        <p class="col-12">صفحات مجازی ما</p>
+
+                        <a href="">
+                            <img src="{{ asset('website/img/social-media-logo/instagram.png') }}" style="max-width: 25px; max-height: 25px;" alt="instagram">
+                        </a>
+                        <a href="">
+                            <img src="{{ asset('website/img/social-media-logo/telegram.svg') }}" alt="telegram">
+                        </a>
+                        <a href="">
+                            <img src="{{ asset('website/img/social-media-logo/whatsup.png') }}" style="max-width: 25px; max-height: 25px;" alt="whatsup">
+                        </a>
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 252 252" width="36px" height="36px">
+                                <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                                    <path d="M0,252v-252h252v252z" fill="none"/>
+                                    <g id="Layer_1" fill="#ffffff">
+                                        <g>
+                                            <path d="M114.87525,32.214l-22.65375,-5.1135c-18.48,-4.17375 -36.83925,7.4235 -41.013,25.9035l-5.16075,22.84275c14.868,-23.60925 39.8265,-40.20975 68.8275,-43.63275z"/>
+                                            <path d="M32.214,137.12475l-5.1135,22.65375c-4.17375,18.48 7.4235,36.83925 25.9035,41.013l22.84275,5.16075c-23.60925,-14.87325 -40.20975,-39.8265 -43.63275,-68.8275z"/>
+                                            <path d="M198.996,51.2085l-22.84275,-5.16075c23.60925,14.868 40.20975,39.8265 43.63275,68.82225l5.1135,-22.65375c4.17375,-18.4695 -7.4235,-36.834 -25.9035,-41.00775z"/>
+                                            <path d="M137.12475,219.786l22.65375,5.1135c18.48,4.17375 36.83925,-7.4235 41.013,-25.9035l5.16075,-22.84275c-14.868,23.60925 -39.8265,40.20975 -68.8275,43.63275z"/>
+                                        </g>
+                                        <path d="M126,42c-46.39425,0 -84,37.60575 -84,84c0,46.39425 37.60575,84 84,84c46.39425,0 84,-37.60575 84,-84c0,-46.39425 -37.60575,-84 -84,-84zM105,68.25c11.59725,0 21,9.40275 21,21c0,11.59725 -9.40275,21 -21,21c-11.59725,0 -21,-9.40275 -21,-21c0,-11.59725 9.40275,-21 21,-21zM89.25,168c-11.59725,0 -21,-9.40275 -21,-21c0,-11.59725 9.40275,-21 21,-21c11.59725,0 21,9.40275 21,21c0,11.59725 -9.40275,21 -21,21zM115.5,126c0,-5.80125 4.69875,-10.5 10.5,-10.5c5.80125,0 10.5,4.69875 10.5,10.5c0,5.80125 -4.69875,10.5 -10.5,10.5c-5.80125,0 -10.5,-4.69875 -10.5,-10.5zM147,183.75c-11.59725,0 -21,-9.40275 -21,-21c0,-11.59725 9.40275,-21 21,-21c11.59725,0 21,9.40275 21,21c0,11.59725 -9.40275,21 -21,21zM162.75,126c-11.59725,0 -21,-9.40275 -21,-21c0,-11.59725 9.40275,-21 21,-21c11.59725,0 21,9.40275 21,21c0,11.59725 -9.40275,21 -21,21z"/>
+                                    </g>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="certificate col-md-12 text-center">
+                <a class="d-inline-block" href="#">
+                    <div style="padding: 1rem; border-radius: 10px; border: 1px solid #e0e0e6; background-color: #fff">
+                        <img src="{{ asset('website/img/certificate/rezi.png') }}" alt="" style="height: 70px; width: 70px;">
+                    </div>
+                </a>
+                <a class="d-inline-block" href="#">
+                    <div style="padding: 1rem; border-radius: 10px; border: 1px solid #e0e0e6; background-color: #fff">
+                        <img src="{{ asset('website/img/certificate/logo.png') }}" alt="" style="height: 70px; width: 70px;">
+                    </div>
+                </a>
+                <a class="d-inline-block" href="#">
+                    <div style="padding: 1rem; border-radius: 10px; border: 1px solid #e0e0e6; background-color: #fff">
+                        <img src="{{ asset('website/img/certificate/kasbokar.png') }}" alt="" style="height: 70px; width: 70px;">
+                    </div>
+                </a>
+            </div>
+    </footer>
+</section>
+<section id="copy-right" class="container-fluid text-center py-2">
+    <p class="m-0">
+        {{-- //todo --}}
+        <a href="#">todo</a>
+    </p>
 </section>
 
 <script src="{{ asset('bootstrap/bootstrap.bundle.min.js') }}"></script>
