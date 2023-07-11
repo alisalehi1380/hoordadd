8b275423a0f53f8aa7390cd598f07003ea35843f
Ali_Salehi <alisalehi13800930@gmail.com>
2023-07-11 12:34:04 +0330

    init project

---
+++ resources/views/website.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>هورداد</title>
+
+    <link rel="stylesheet" href="{{ asset('website/bootstrap/bootstrap.rtl.css') }}">
+    <link rel="stylesheet" href="{{ asset('website/css/styleWebsite.css') }}">
+</head>
+<body>
+<section id="body" class="container">
+    <section>
+        <nav>
+            <div>
+                <a><img src="#"></a>
+                <input value="s">
+
+            </div>
+            <div></div>
+        </nav>
+    </section>
+
+    <section id="content">
+        <div id="category">
+
+        </div>
+
+        <div id="newest-products">
+
+        </div>
+    </section>
+</section>
+
+<script src="{{ asset('website/bootstrap/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
